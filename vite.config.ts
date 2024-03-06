812a2663fafae91b1fb465291ba148b8c60849c4
alix2018 <stephanie.alix95@gmail.com>
2024-03-06 11:21:39 +0100

    Setup PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,28 @@ export default defineConfig({
           dest: './'
         }
       ]
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Train Mexicain',
+        short_name: 'Train',
+        description: 'Application pour le jeu du train Mexicain',
+        theme_color: '#0f7ec4',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     })
   ],
   resolve: {
