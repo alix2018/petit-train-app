445111d2a7a66c5ba29d798e1a40f051ae3ceaaa
alix2018 <stephanie.alix95@gmail.com>
2026-04-23 13:55:57 +0200

    Use dominos for tour title

--- src/components/CountPoints.vue
+++ src/components/CountPoints.vue
@@ -80,7 +80,11 @@ function deletePlayer(player: Player) {
 
 <template>
   <template v-if="gameStore.gameStarted">
-    <h1 v-if="!isResultsPage">Tour : Double {{ gameStore.roundCounter }}</h1>
+    <img
+      v-if="!isResultsPage"
+      :src="`/src/assets/double${gameStore.roundCounter}.png`"
+      height="70px"
+    />
     <h1 v-else>🎉 Résultats 🎉</h1>
   </template>
 
